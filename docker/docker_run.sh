c43f01c74a3189ed921c6cabed074d2a0d5ec5fd
Baasit <abdulbaasitsanusi@gmail.com>
2026-04-06 19:55:53 +0100

    minor change to docker.sh

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -7,7 +7,7 @@ echo "Make sure you are in the slambook2 root directory."
 docker run -it \
   -e DISPLAY="$DISPLAY" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /home/jacobs/ws/slambook2:/slambook2 \
+  -v $PWD:/slambook2 \
   --gpus all \
   --name slambook2 \
   slambook2:latest \
